8987aefc440ff5dc2bcf8e8c2443008191d6b1a4
Bumbuk <144329036+Bumbuk@users.noreply.github.com>
2026-02-09 23:34:42 +0300

    Delete components directory

--- components/layout/Container.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container(props: { children: React.ReactNode; className?: string }) {
-  return (
-    <div className={"mx-auto w-full max-w-6xl px-4 " + (props.className ?? "")}>
-      {props.children}
-    </div>
-  );
-}
